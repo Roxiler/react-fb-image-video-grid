ca745739d0795edaccf9bf3e6b940d2a791c8d22
sai6855 <60743144+sai6855@users.noreply.github.com>
2022-02-09 23:33:51 +0530

    added carausel support

--- src/Library/Components/Carousel/index.tsx
+++ src/Library/Components/Carousel/index.tsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+import Modal from "react-modal";
+import { clsx } from "../../Utilis/clsx";
+import classes from "./styles.module.css";
 
-const index = () => {
+type CarauselType = {
+  images: JSX.Element[];
+  initialImageIndex: number;
+  isOpen: boolean;
+  onClose: any;
+};
+
+const Carausel = ({
+  images,
+  initialImageIndex,
+  isOpen,
+  onClose,
+}: CarauselType) => {
+  const [currentImageIndex, setCurrentImageIndex] = React.useState(
+    () => images.findIndex((_, i) => i === initialImageIndex) as number
+  );
+
+  React.useEffect(() => {
+    setCurrentImageIndex(
+      () => images.findIndex((_, i) => i === initialImageIndex) as number
+    );
+    //eslint-disable-next-line
+  }, [initialImageIndex]);
+
+  const currentImage = images[currentImageIndex];
+
   return (
-    <div>index</div>
-  )
-}
+    <Modal
+      ariaHideApp={false}
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      contentLabel="Example Modal"
+    >
+      <div className={classes.modal_wrap}>
+        <span
+          onClick={() =>
+            setCurrentImageIndex(
+              currentImageIndex === 0
+                ? images.length - 1
+                : currentImageIndex - 1
+            )
+          }
+        >
+          P
+        </span>
+        {React.cloneElement(currentImage, {
+          className: clsx(classes.img, currentImage.props.className || ""),
+        })}
+        <span
+          onClick={() =>
+            setCurrentImageIndex(
+              currentImageIndex === images.length - 1
+                ? 0
+                : currentImageIndex + 1
+            )
+          }
+        >
+          N
+        </span>
+      </div>
+    </Modal>
+  );
+};
 
-export default index
+export default Carausel;
--- src/Library/Components/ImageGrid/index.tsx
+++ src/Library/Components/ImageGrid/index.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from "react";
 import { clsx } from "../../Utilis/clsx";
+import Carausel from "../Carousel";
 import classes from "./styles.module.css";
 
 interface Props {
@@ -10,40 +11,52 @@ interface Props {
 const ImageGrid = ({ children, className = "" }: Props) => {
   const numberOfImages = Array.isArray(children) ? children.length : 1;
 
-  const imageRefs = React.useRef<HTMLImageElement[]>([]);
+  const [selectedImageIndex, setSelectedImageIndex] = React.useState<number>(0);
+  const [isOpenCarausel, setIsOpenCarausel] = React.useState(false);
 
-  const [imageSrcs, setImgSrcs] = React.useState<string[]>([]);
+  const handleImageClick = (imageIndex: number) => {
+    setIsOpenCarausel(true);
+    return imageIndex;
+  };
 
-  React.useEffect(() => {
-    imageRefs.current.forEach((img) => {
-      setImgSrcs((prevState) => [...prevState, img.src]);
-    });
-  }, []);
-
-  console.log(imageSrcs);
+  const handleCloseCarausel = () => setIsOpenCarausel(false);
 
   if (numberOfImages < 5) {
     return (
-      <div
-        className={clsx(
-          classes.grid_wrapper,
-          classes[`grid_wrapper_${numberOfImages}_img`],
-          className
-        )}
-      >
-        {React.Children.map(children, (child, i) => {
-          const imgCount = i + 1;
-          return React.cloneElement(child as ReactElement, {
-            ...child.props,
-            ref: (el: HTMLImageElement) => {
-              imageRefs.current = [...imageRefs.current, el];
-            },
-            className: ` ${classes.img} ${
-              classes[`img_${numberOfImages}_${imgCount}`]
-            } ${child.props?.className || ""}`,
-          });
-        })}
-      </div>
+      <>
+        <Carausel
+          isOpen={isOpenCarausel}
+          images={
+            numberOfImages === 1
+              ? [children as JSX.Element]
+              : (children as JSX.Element[])
+          }
+          initialImageIndex={selectedImageIndex}
+          onClose={handleCloseCarausel}
+        />
+
+        <div
+          className={clsx(
+            classes.grid_wrapper,
+            classes[`grid_wrapper_${numberOfImages}_img`],
+            className
+          )}
+        >
+          {React.Children.map(children, (child, i) => {
+            const imgCount = i + 1;
+            return React.cloneElement(child as ReactElement, {
+              ...child.props,
+              className: ` ${classes.img} ${
+                classes[`img_${numberOfImages}_${imgCount}`]
+              } ${child.props?.className || ""}`,
+              onClick: () => {
+                child.props.onClick?.();
+                setSelectedImageIndex(handleImageClick(i));
+              },
+            });
+          })}
+        </div>
+      </>
     );
   } else {
     const firstRow = (children as JSX.Element[]).filter((_, i) => i < 2);
@@ -52,46 +65,61 @@ const ImageGrid = ({ children, className = "" }: Props) => {
     const secondRowImgCount = secondRow.length;
 
     return (
-      <div className={clsx(classes.sub_grid_wrapper, className)}>
-        <div
-          className={clsx(
-            classes.sub_grid_wrapper_first_row,
-            classes[`grid_wrapper_${firstRow.length}_img`]
-          )}
-        >
-          {React.Children.map(firstRow, (child, i) => {
-            const imgCount = i + 1;
-            return React.cloneElement(child as ReactElement, {
-              ...child.props,
-              ref: (el: HTMLImageElement) => {
-                imageRefs.current = [...imageRefs.current, el];
-              },
-              className: ` ${classes.img} ${
-                classes[`img_${firstRowImgCount}_${imgCount}`]
-              } ${child.props?.className || ""}`,
-            });
-          })}
+      <>
+        <Carausel
+          isOpen={isOpenCarausel}
+          initialImageIndex={selectedImageIndex}
+          images={
+            numberOfImages === 1
+              ? [children as JSX.Element]
+              : (children as JSX.Element[])
+          }
+          onClose={handleCloseCarausel}
+        />
+
+        <div className={clsx(classes.sub_grid_wrapper, className)}>
+          <div
+            className={clsx(
+              classes.sub_grid_wrapper_first_row,
+              classes[`grid_wrapper_${firstRow.length}_img`]
+            )}
+          >
+            {React.Children.map(firstRow, (child, i) => {
+              const imgCount = i + 1;
+              return React.cloneElement(child as ReactElement, {
+                ...child.props,
+                className: ` ${classes.img} ${
+                  classes[`img_${firstRowImgCount}_${imgCount}`]
+                } ${child.props?.className || ""}`,
+                onClick: () => {
+                  child.props.onClick?.();
+                  setSelectedImageIndex(handleImageClick(i));
+                },
+              });
+            })}
+          </div>
+          <div
+            className={clsx(
+              classes.sub_grid_wrapper_second_row,
+              classes[`sub_grid_wrapper_${secondRow.length}_img`]
+            )}
+          >
+            {React.Children.map(secondRow, (child, i) => {
+              const imgCount = i + 1;
+              return React.cloneElement(child as ReactElement, {
+                ...child.props,
+                className: ` ${classes.img} ${
+                  classes[`sub_img_${secondRowImgCount}_${imgCount}`]
+                } ${child.props?.className || ""}`,
+                onClick: () => {
+                  child.props.onClick?.();
+                  setSelectedImageIndex(handleImageClick(i + 2));
+                },
+              });
+            })}
+          </div>
         </div>
-        <div
-          className={clsx(
-            classes.sub_grid_wrapper_second_row,
-            classes[`sub_grid_wrapper_${secondRow.length}_img`]
-          )}
-        >
-          {React.Children.map(secondRow, (child, i) => {
-            const imgCount = i + 1;
-            return React.cloneElement(child as ReactElement, {
-              ...child.props,
-              ref: (el: HTMLImageElement) => {
-                imageRefs.current = [...imageRefs.current, el];
-              },
-              className: ` ${classes.img} ${
-                classes[`sub_img_${secondRowImgCount}_${imgCount}`]
-              } ${child.props?.className || ""}`,
-            });
-          })}
-        </div>
-      </div>
+      </>
     );
   }
 };
