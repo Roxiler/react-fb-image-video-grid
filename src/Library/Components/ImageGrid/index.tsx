73cfe34b839e2e4d64b79770c7796892e87c723d
sai6855 <60743144+sai6855@users.noreply.github.com>
2022-02-09 11:09:50 +0530

    added 5 images support

--- src/Library/Components/ImageGrid/index.tsx
+++ src/Library/Components/ImageGrid/index.tsx
@@ -10,25 +10,71 @@ interface Props {
 const ImageGrid = ({ children, className = "" }: Props) => {
   const numberOfImages = Array.isArray(children) ? children.length : 1;
 
-  return (
-    <div
-      className={clsx(
-        classes.grid_wrapper,
-        classes[`grid_wrapper_${numberOfImages}_img`],
-        className
-      )}
-    >
-      {React.Children.map(children, (child, i) => {
-        const imgCount = i + 1;
-        return React.cloneElement(child as ReactElement, {
-          ...child.props,
-          className: ` ${classes.img} ${
-            classes[`img_${numberOfImages}_${imgCount}`]
-          } ${child.props?.className || ""}`,
-        });
-      })}
-    </div>
-  );
+  if (numberOfImages < 5) {
+    return (
+      <div
+        className={clsx(
+          classes.grid_wrapper,
+          classes[`grid_wrapper_${numberOfImages}_img`],
+          className
+        )}
+      >
+        {React.Children.map(children, (child, i) => {
+          const imgCount = i + 1;
+          return React.cloneElement(child as ReactElement, {
+            ...child.props,
+            className: ` ${classes.img} ${
+              classes[`img_${numberOfImages}_${imgCount}`]
+            } ${child.props?.className || ""}`,
+          });
+        })}
+      </div>
+    );
+  } else {
+    const firstRow = (children as JSX.Element[]).filter((_, i) => i < 2);
+    const firstRowImgCount = firstRow.length;
+    const secondRow = (children as JSX.Element[]).filter((_, i) => i >= 2);
+    const secondRowImgCount = secondRow.length;
+
+    console.log({ firstRow, firstRowImgCount, secondRow, secondRowImgCount });
+
+    return (
+      <div className={clsx(classes.sub_grid_wrapper, className)}>
+        <div
+          className={clsx(
+            classes.sub_grid_wrapper_row,
+            classes[`grid_wrapper_${firstRow.length}_img`]
+          )}
+        >
+          {React.Children.map(firstRow, (child, i) => {
+            const imgCount = i + 1;
+            return React.cloneElement(child as ReactElement, {
+              ...child.props,
+              className: ` ${classes.img} ${
+                classes[`img_${firstRowImgCount}_${imgCount}`]
+              } ${child.props?.className || ""}`,
+            });
+          })}
+        </div>
+        <div
+          className={clsx(
+            classes.sub_grid_wrapper_row,
+            classes[`sub_grid_wrapper_${secondRow.length + 1}_img`]
+          )}
+        >
+          {React.Children.map(secondRow, (child, i) => {
+            const imgCount = i + 1;
+            return React.cloneElement(child as ReactElement, {
+              ...child.props,
+              className: ` ${classes.img} ${
+                classes[`sub_img_${secondRowImgCount}_${imgCount}`]
+              } ${child.props?.className || ""}`,
+            });
+          })}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default ImageGrid;
